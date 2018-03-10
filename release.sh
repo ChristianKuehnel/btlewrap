e27fa17d733634c20b3b1f9e9b252700619ce88d
Christian Kühnel <christian.kuehnel@gmail.com>
2018-03-10 16:15:28 +0100

    extended release script

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # release current branch to pypi
 
+echo Releasing version:
+grep version setup.py
+read -n1 -r -p "Press any key to continue..." key
+
 rm -r dist
 python3 setup.py sdist
 twine upload dist/*
