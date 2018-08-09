9b8d5abf7ea61f40567028e5aca2569ff94af49c
Christian Kühnel <christian.kuehnel@gmail.com>
2018-08-09 12:00:44 +0200

    added test for exception in with statement

--- test/unit_tests/test_bluetooth_interface.py
+++ test/unit_tests/test_bluetooth_interface.py
@@ -16,3 +16,12 @@ class TestBluetoothInterface(unittest.TestCase):
             self.assertTrue(bluetooth_if.is_connected())
 
         self.assertFalse(bluetooth_if.is_connected())
+
+    def test_exception_in_with(self):
+        """Test clean exit after exception."""
+        bluetooth_if = BluetoothInterface(MockBackend)
+        self.assertFalse(bluetooth_if.is_connected())
+        with self.assertRaises(ValueError):
+            with bluetooth_if.connect('abc'):
+                raise ValueError('some test exception')
+        self.assertFalse(bluetooth_if.is_connected())
