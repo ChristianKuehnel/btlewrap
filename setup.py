33bba66a335387ea8f38fed4d547c08b77ec8ad3
Christian Kühnel <christian.kuehnel@gmail.com>
2019-09-28 11:19:37 +0200

    removed python 3.8

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     packages=find_packages(),
     keywords='bluetooth low-energy ble',
