19e352034d9dbf85b92377c8f5f1c5ef00c83428
ChristianKuehnel <christian.kuehnel@gmail.com>
2019-10-06 08:54:00 +0200

    bumped bluepy version number to 1.3.0 (#18)
    * bumped bluepy version number to 1.3.0
    * added missing pexpect dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     zip_safe=False,
     extras_require={
         'testing': ['pytest'], 
-        'bluepy': ['bluepy==1.2.0'], 
+        'bluepy': ['bluepy==1.3.0'],
         'pygatt': ['pygatt==3.4.0'], 
         },
     install_requires=['typing>=3,<4'],
