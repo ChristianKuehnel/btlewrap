44091cad1b4209de6d42cceb857d50b755487fcb
Christian Kühnel <christian.kuehnel@gmail.com>
2019-10-06 10:35:30 +0200

    added primitive integration tests

---
+++ test/integration_tests/test_gatttool.py
@@ -0,0 +1,20 @@
+"""These btlewrap by connecting to a real device."""
+
+import unittest
+import pytest
+from btlewrap import GatttoolBackend
+
+
+class TestGatttool(unittest.TestCase):
+    # pylint does not understand pytest fixtures, so we have to disable the warning
+    # pylint: disable=no-member
+
+    def setUp(self):
+        """Set up the test environment."""
+        self.backend = GatttoolBackend()
+
+    @pytest.mark.usefixtures("mac")
+    def test_connect(self):
+        """Try connecting to a device."""
+        self.backend.connect(self.mac)
+        self.backend.disconnect()
