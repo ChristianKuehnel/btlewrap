0cbc85cca42425a391e65f3f6754410b8da3ef64
Christian Kühnel <christian.kuehnel@gmail.com>
2019-10-12 08:51:54 +0200

    removed python 3.4 again

--- btlewrap/__init__.py
+++ btlewrap/__init__.py
@@ -4,7 +4,7 @@ from btlewrap.version import __version__  # noqa: F401
 
 # This check must be run first, so that it fails before loading the other modules.
 # Otherwise we do not get a clean error message.
-if sys.version_info <= (3, 4):
+if sys.version_info < (3, 4):
     raise ValueError('this library requires at least Python 3.4. ' +
                      'You\'re running version {}.{} from {}.'.format(
                          sys.version_info.major,
